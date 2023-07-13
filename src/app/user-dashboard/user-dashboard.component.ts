0d9f846ecf456e0d8baf325648f7b0a8b0a99393
Natika31 <natika.orsas@gmail.com>
2023-07-13 15:55:19 +0200

    userDashboardComponent

---
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FileViewerComponent } from '../file-viewer/file-viewer.component';
+import { MyFile } from '../file-viewer/MyFile';
+
+@Component({
+  selector: 'app-user-dashboard',
+  templateUrl: './user-dashboard.component.html',
+  styleUrls: ['./user-dashboard.component.css'],
+  standalone: true,
+  imports: [MatTabsModule, FileViewerComponent],
+})
+export class UserDashboardComponent {
+  //TODO: create parent directory and display contents
+  selectedFile: MyFile = {
+    file_id: 1,
+    file_name: 'Titre',
+    file_format: '',
+    file_path: '/',
+  };
+}
